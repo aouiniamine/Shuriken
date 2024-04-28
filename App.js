b40f8e9f8cedaa18c60162e52b7a8aa9cbcd1428
amineaouini-notbot <aminaouini565@gmail.com>
2024-04-28 14:34:56 +0100

    loop

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Animated, Easing, StyleSheet, Text, View } from 'react-native';
 import Shuriken from './components/Shuriken';
 import { useEffect, useRef } from 'react';
 
 export default function App() {
   const rotationVal = useRef(new Animated.Value(0)).current
   useEffect(()=>{
-    Animated.timing(rotationVal, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start()
-  })
+    Animated.loop(
+      Animated.sequence([
+
+        Animated.timing(rotationVal, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+          easing: Easing.linear
+        }),
+      ])
+
+    ).start()
+  }, [])
   const spin = rotationVal.interpolate({
     inputRange: [0, 1],
     outputRange: ["0deg", "360deg"]
